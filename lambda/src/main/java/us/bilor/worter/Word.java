121f78e5ed23dbd7208af8953738c744fea34d8f
Andrii Bilorus <andrii.bilorus@gmail.com>
2018-04-15 14:18:20 +0300

    remove lamdba code

--- lambda/src/main/java/us/bilor/worter/Word.java
+++
@@ -1,9 +0,0 @@
-package us.bilor.worter;
-
-import com.amazonaws.services.lambda.runtime.Context;
-
-public class Word {
-  public String addWord(String name, Context context) {
-    return "";
-  }
-}
